83fa6dcdb0c4e9114fdee75c4bc913fb0308b43f
Nyamosi <58230604+Jose-Nyamos@users.noreply.github.com>
2022-04-16 19:52:35 +0300

    config

--- server/sql/config.js
+++ server/sql/config.js
@@ -1,8 +1,8 @@
 let config = {
-    host:'us-cdbr-east-05.cleardb.net/',
-    user:'bfea4a06d17854',
-    password:'156c9b1f',
-    database:'heroku_e18be060a62c305'
+    host:'localhost',
+    user:'onlinebookingus',
+    password:'onlinebookingus',
+    database:'booking_and_reservation'
 }
 
 module.exports = config
